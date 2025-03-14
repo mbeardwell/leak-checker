c63199d9fdb038c3187ae434f137f6d923298946
mbeardwell <matthewbeardwell@gmail.com>
2025-03-14 04:20:38 +0000

    Implemented HTML search results --> object list

--- web_scrape.py
+++ web_scrape.py
@@ -1,20 +1,58 @@
 #!/bin/python3
 
 import requests
-import urllib.parse
+from bs4 import BeautifulSoup
 
-DDG_URL = 'https://duckduckgo.com/?q='
+class Result():
+	def __init__(self, title, url, description):
+		self.title = title
+		self.url = url
+		self.description = description
 
+	def print(self):
+		print('=== Result ===')
+		print(f'\t Title: {self.title}')
+		print(f'\t URL: {self.url}')
+		print(f'\t Description: {self.description}')
 
-def generate_url(engine, query_str):
+	def get_title(self):
+		return self.title
 
-	if engine == 'duckduckgo':
-		params = urllib.parse.urlencode({'q': query_str})
-		return DDG_URL + params
+	def get_url(self):
+		return self.url
 
-	return None
+	def get_description(self):
+		return self.description
 
-url = generate_url('duckduckgo', 'site:pastebin.com Hello.com')
-request = requests.get(url)
+def parse_results(html):
+	TITLE_CLASS = 'result__a'
+	URL_CLASS = 'result__url'
+	DESCRIPTION_CLASS = 'result__snippet'
+
+	soup = BeautifulSoup(html, 'html.parser')
+
+	results_html = soup.find_all("div", class_="result")
+	results = []
+
+	for html in results_html:
+		title = html.find('a', class_=TITLE_CLASS).text.strip()
+		url = html.find('a', class_=URL_CLASS).text.strip()
+		description = html.find('a', class_=DESCRIPTION_CLASS).text.strip()
+		results.append(Result(title, url, description))
+
+	for r in results:
+		r.print()
+
+	return results
+
+USER_AGENT = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Mobile Safari/537.36'
+DDG_URL = 'https://duckduckgo.com/html/?'
+
+query = 'test'
+headers = {'user-agent': USER_AGENT}
+request = requests.get(DDG_URL + f'q={query}', headers=headers)
+
 print(request.status_code)
-print(request.text)
+print(request.url)
+
+results = parse_results(request.text)
